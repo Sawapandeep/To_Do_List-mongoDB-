9742c0322748efcf3097f146907e54b053b64ec3
Sawapandeep Singh <sawapandeep2089.be20@chitkara.edu.in>
2022-12-18 19:59:04 +0530

    first

---
+++ models/todo.js
@@ -0,0 +1,17 @@
+const mongoose =require("mongoose"); 
+const Schema = mongoose.Schema;
+
+const todoSchema= new Schema({
+    user:{
+        type:String,
+        required:true
+    },
+    todo:{
+        type:String,
+        required:true
+    }
+
+})
+
+const todo=mongoose.model("todo",todoSchema)
+module.exports=todo;
